ce831e1cdbc8f941156d9eee41cb6d23f409fb73
jmservic <Jmhservice@gmail.com>
2026-02-04 00:02:07 -0500

    updated init.lua to prevent autocompletion from selecting a suggestion immediately, and added the ability to use <Tab> and <ctrl-space> to trigger autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+vim.cmd([[set completeopt+=menuone,noselect]])
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+
+-- gopls lps configuration
 vim.lsp.enable('gopls')
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp', {}),
@@ -11,7 +18,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
       -- Optional: trigger autocompletion on EVERY keypress. May be slow!
       -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       -- client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+		vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+		vim.keymap.set('i', '<c-space>', function()
+			vim.lsp.completion.get()
+			end
+		)
+		vim.keymap.set('i', '<Tab>', function()
+				local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+				local line = vim.api.nvim_get_current_line()
+				local char = line:sub(col, col)
+
+				if vim.re.find(char, '%w') then
+					vim.lsp.completion.get()
+				else
+					vim.api.nvim_put({'\t'}, "c", false, true)
+				end
+			end
+		)
     end
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
